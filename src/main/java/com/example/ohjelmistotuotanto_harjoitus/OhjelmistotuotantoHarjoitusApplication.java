eb6df3487ec2995697a47f954e4d4aaf822d6cff
KaiKatajisto <kai.katajisto@seamk.fi>
2026-02-13 18:11:45 +0200

    Initiaali spring boot 3.5.10 luuranko.

---
+++ src/main/java/com/example/ohjelmistotuotanto_harjoitus/OhjelmistotuotantoHarjoitusApplication.java
@@ -0,0 +1,13 @@
+package com.example.ohjelmistotuotanto_harjoitus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhjelmistotuotantoHarjoitusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OhjelmistotuotantoHarjoitusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ohjelmistotuotanto_harjoitus/OhjelmistotuotantoHarjoitusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ohjelmistotuotanto_harjoitus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhjelmistotuotantoHarjoitusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
