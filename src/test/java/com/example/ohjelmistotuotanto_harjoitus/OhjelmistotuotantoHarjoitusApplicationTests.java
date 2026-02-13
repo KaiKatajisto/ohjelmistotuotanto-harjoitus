c6999c5abb4629e69cd4a7835f9a402c85ecd64e
KaiKatajisto <kai.katajisto@seamk.fi>
2026-02-13 22:48:37 +0200

    fix: Lisätty puuttuva import ActiveProfilesille

--- src/test/java/com/example/ohjelmistotuotanto_harjoitus/OhjelmistotuotantoHarjoitusApplicationTests.java
+++ src/test/java/com/example/ohjelmistotuotanto_harjoitus/OhjelmistotuotantoHarjoitusApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.ohjelmistotuotanto_harjoitus;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
